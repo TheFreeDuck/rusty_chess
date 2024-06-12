1921cd0db6999b4916d02d182814d6c2112a61e3
TheFreeDuck <fredrikthorcewers@gmail.com>
2024-06-12 19:23:18 +0200

    added has_moved bool to rook and king

--- src/chess.rs
+++ src/chess.rs
@@ -1,14 +1,13 @@
-use std::collections::btree_set::Difference;
-
+use std::collections::HashMap;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 enum Piece {
-    Pawn(Color),
-    Knight(Color),
-    Bishop(Color),
-    Rook(Color),
-    Queen(Color),
-    King(Color),
+    Pawn{color: Color},
+    Knight{color: Color},
+    Bishop{color: Color},
+    Rook{color: Color, has_moved: bool},
+    Queen{color: Color},
+    King{color: Color, has_moved: bool},
 }
 
 impl Piece {
@@ -20,12 +19,12 @@ impl Piece {
         }
 
         match self {
-            Piece::Pawn(color) => Piece::is_legal_pawn_move(from, to, board, *color),
-            Piece::Knight(color) => Piece::is_legal_knight_move(from, to, board, *color),
-            Piece::Bishop(color) => Piece::is_legal_bishop_move(from, to, board, *color),
-            Piece::Rook(color) => Piece::is_legal_rook_move(from, to, board, *color),
-            Piece::Queen(color) => Piece::is_legal_queen_move(from, to, board, *color),
-            Piece::King(color) => Piece::is_legal_king_move(from, to, board, *color),
+            Piece::Pawn{color} => Piece::is_legal_pawn_move(from, to, board, *color),
+            Piece::Knight{color} => Piece::is_legal_knight_move(from, to, board, *color),
+            Piece::Bishop{color} => Piece::is_legal_bishop_move(from, to, board, *color),
+            Piece::Rook{color,..} => Piece::is_legal_rook_move(from, to, board, *color),
+            Piece::Queen{color} => Piece::is_legal_queen_move(from, to, board, *color),
+            Piece::King{color,..} => Piece::is_legal_king_move(from, to, board, *color),
         }
     }
 
@@ -185,12 +184,12 @@ impl Piece {
 
     fn get_color(&self) -> Color {
         match self {
-            Piece::Pawn(color) => *color,
-            Piece::Knight(color) => *color,
-            Piece::Bishop(color) => *color,
-            Piece::Rook(color) => *color,
-            Piece::Queen(color) => *color,
-            Piece::King(color) => *color,
+            Piece::Pawn{color} => *color,
+            Piece::Knight{color} => *color,
+            Piece::Bishop{color} => *color,
+            Piece::Rook{color,..} => *color,
+            Piece::Queen{color} => *color,
+            Piece::King{color,..} => *color,
         }
     }
 
@@ -202,7 +201,7 @@ enum Color {
     White,
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Eq, Hash)]
 pub struct Coordinate{
     x: i32,
     y: i32,
@@ -248,44 +247,46 @@ impl Board {
     pub fn starting_positions() -> Board {
         let mut squares = [[None; 8]; 8];
 
-        squares[0][0] = Some(Piece::Rook(Color::White));
-        squares[1][0] = Some(Piece::Knight(Color::White));
-        squares[2][0] = Some(Piece::Bishop(Color::White));
-        squares[3][0] = Some(Piece::Queen(Color::White));
-        squares[4][0] = Some(Piece::King(Color::White));
-        squares[5][0] = Some(Piece::Bishop(Color::White));
-        squares[6][0] = Some(Piece::Knight(Color::White));
-        squares[7][0] = Some(Piece::Rook(Color::White));
+        let positions = vec![
+            (0, 0, Piece::Rook { color: Color::White, has_moved: false }),
+            (1, 0, Piece::Knight { color: Color::White }),
+            (2, 0, Piece::Bishop { color: Color::White }),
+            (3, 0, Piece::Queen { color: Color::White }),
+            (4, 0, Piece::King { color: Color::White, has_moved: false }),
+            (5, 0, Piece::Bishop { color: Color::White }),
+            (6, 0, Piece::Knight { color: Color::White }),
+            (7, 0, Piece::Rook { color: Color::White, has_moved: false }),
+            (0, 1, Piece::Pawn { color: Color::White }),
+            (1, 1, Piece::Pawn { color: Color::White }),
+            (2, 1, Piece::Pawn { color: Color::White }),
+            (3, 1, Piece::Pawn { color: Color::White }),
+            (4, 1, Piece::Pawn { color: Color::White }),
+            (5, 1, Piece::Pawn { color: Color::White }),
+            (6, 1, Piece::Pawn { color: Color::White }),
+            (7, 1, Piece::Pawn { color: Color::White }),
+        
+            (0, 7, Piece::Rook { color: Color::Black, has_moved: false }),
+            (1, 7, Piece::Knight { color: Color::Black }),
+            (2, 7, Piece::Bishop { color: Color::Black }),
+            (3, 7, Piece::Queen { color: Color::Black }),
+            (4, 7, Piece::King { color: Color::Black, has_moved: false }),
+            (5, 7, Piece::Bishop { color: Color::Black }),
+            (6, 7, Piece::Knight { color: Color::Black }),
+            (7, 7, Piece::Rook { color: Color::Black, has_moved: false }),
+            (0, 6, Piece::Pawn { color: Color::Black }),
+            (1, 6, Piece::Pawn { color: Color::Black }),
+            (2, 6, Piece::Pawn { color: Color::Black }),
+            (3, 6, Piece::Pawn { color: Color::Black }),
+            (4, 6, Piece::Pawn { color: Color::Black }),
+            (5, 6, Piece::Pawn { color: Color::Black }),
+            (6, 6, Piece::Pawn { color: Color::Black }),
+            (7, 6, Piece::Pawn { color: Color::Black }),
+        ];
 
-        for i in 0..8 {
-            squares[i][1] = Some(Piece::Pawn(Color::White));
+        for (x,y, piece) in positions{
+            squares[x][y] = Some(piece);
         }
 
-        squares[0][7] = Some(Piece::Rook(Color::Black));
-        squares[1][7] = Some(Piece::Knight(Color::Black));
-        squares[2][7] = Some(Piece::Bishop(Color::Black));
-        squares[3][7] = Some(Piece::Queen(Color::Black));
-        squares[4][7] = Some(Piece::King(Color::Black));
-        squares[5][7] = Some(Piece::Bishop(Color::Black));
-        squares[6][7] = Some(Piece::Knight(Color::Black));
-        squares[7][7] = Some(Piece::Rook(Color::Black));
-
-        for i in 0..8 {
-            squares[i][6] = Some(Piece::Pawn(Color::Black));
-        }
-
-        Board {
-            squares,
-            side_to_move: Color::White,
-        }
-    }
-
-    pub fn rook() -> Board {
-        let mut squares = [[None; 8]; 8];
-        squares[0][7] = Some(Piece::Rook(Color::White));
-
-        squares[0][1] = Some(Piece::Rook(Color::White));
-        squares[6][7] = Some(Piece::Rook(Color::White));
         Board {
             squares,
             side_to_move: Color::White,
@@ -318,19 +319,19 @@ impl Board {
 
                 // read color of piece here and set the color of text
                 match piece {
-                    Some(Piece::Pawn(Color::Black)) => print!("[p]"),
-                    Some(Piece::Knight(Color::Black)) => print!("[n]"),
-                    Some(Piece::Bishop(Color::Black)) => print!("[b]"),
-                    Some(Piece::Rook(Color::Black)) => print!("[r]"),
-                    Some(Piece::Queen(Color::Black)) => print!("[q]"),
-                    Some(Piece::King(Color::Black)) => print!("[k]"),
+                    Some(Piece::Pawn{color: Color::Black}) => print!("[p]"),
+                    Some(Piece::Knight{color: Color::Black}) => print!("[n]"),
+                    Some(Piece::Bishop{color: Color::Black}) => print!("[b]"),
+                    Some(Piece::Rook{color: Color::Black,..}) => print!("[r]"),
+                    Some(Piece::Queen{color: Color::Black}) => print!("[q]"),
+                    Some(Piece::King{color: Color::Black,..}) => print!("[k]"),
 
-                    Some(Piece::Pawn(Color::White)) => print!("[P]"),
-                    Some(Piece::Knight(Color::White)) => print!("[N]"),
-                    Some(Piece::Bishop(Color::White)) => print!("[B]"),
-                    Some(Piece::Rook(Color::White)) => print!("[R]"),
-                    Some(Piece::Queen(Color::White)) => print!("[Q]"),
-                    Some(Piece::King(Color::White)) => print!("[K]"),
+                    Some(Piece::Pawn{color: Color::White}) => print!("[P]"),
+                    Some(Piece::Knight{color: Color::White}) => print!("[N]"),
+                    Some(Piece::Bishop{color: Color::White}) => print!("[B]"),
+                    Some(Piece::Rook{color: Color::White,..}) => print!("[R]"),
+                    Some(Piece::Queen{color: Color::White}) => print!("[Q]"),
+                    Some(Piece::King{color: Color::White,..}) => print!("[K]"),
                     None => print!("[ ]"),
                 }
             }
